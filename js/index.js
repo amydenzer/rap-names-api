626677abb8d188abbcfb81f3b14209e18dc53a2c
amydenzer <amydenzer@gmail.com>
2023-09-28 17:36:55 -0500

    cyclic changes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api-jn0l.onrender.com/api/${rapperName}`)
+        const response = await fetch(`https://good-swimsuit-bat.cyclic.cloud/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
